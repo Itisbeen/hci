d3d032333e2cf6f95138e85c6a68e726fb650405
heo <heo@heos-MacBook-Air.local>
2025-11-18 17:57:24 +0900

    main

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 from typing import List, Optional
+import schemas
 
-# class Report(shemas.ReportBase):
+# class Report(schemas.ReportBase):
 #     id: int
 #     enterprise_id: int
 
@@ -30,7 +31,7 @@ def read_item(item_id: int, q: str | None = None):
 #    이것이 웹사이트의 첫 페이지가 됩니다.
 @app.get("/", response_class=FileResponse)
 async def read_index():
-    return "static/index.html"
+    return "static/theme/index.html"
 
 # @app.get("/api/reports", response_model = PaginatedReprotResponse)
 # def get_reports_with_pagination(
@@ -67,7 +68,7 @@ async def read_index():
 #    이 코드가 가장 중요합니다. 이 코드로 인해 index.html 안에 있는
 #    <link href="/css/style.css"> 와 같은 경로 요청을 FastAPI가 받으면
 #    'static' 폴더 안에서 css/style.css 파일을 찾아서 제공하게 됩니다.
-#
+
 #    주의: 이 mount 코드는 다른 모든 경로(@app.get 등)보다 뒤에 위치해야 합니다!
 #    FastAPI는 코드가 작성된 순서대로 경로를 확인하기 때문입니다.
 app.mount("/", StaticFiles(directory="static"), name="static")
